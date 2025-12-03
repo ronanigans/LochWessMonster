1ecc23ccf3df869aefa15d72985107c3811654c0
ronanigans <ronanthesheehan@gmail.com>
2025-12-03 12:26:48 -0500

    fixed playermove

--- Assets/playerMove.cs
+++ Assets/playerMove.cs
@@ -1,52 +1,75 @@
 using UnityEngine;
-using UnityEngine.Events;
-using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class EnemyFollow : MonoBehaviour
+
+public class playerMove : MonoBehaviour
 {
-    public Transform player;
-    public float enemySpeed = 2f;
-    public float slowedSpeed = 0f;
-    public float baseSpeed = 2f;
-    public UnityEvent onHitPlayer;
-    public UnityEvent onDeath;
-    public float charges = 5f;
-    public float timer = 30f;
-    public float targetTime = 10.0f;
-    public bool timerRunning = false;
+    public CharacterController Cc;
+    public Transform cameraTransform;
+    public float Gravity;
+    public float WalkSpeed;
+    private float yspeed;
+    public string enemy = "Enemy";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.visible = false;
     }
+
+
+    public void ResetCurrentScene()
+    {
+        // Get the name of the currently active scene
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+
+        // Load the scene by its name
+        SceneManager.LoadScene(currentSceneName);
+    }
+     void OnTriggerEnter(Collider Col) {
+          if (Col.tag == "Enemy")
+               ResetCurrentScene();
+     }
+
+
+     private void HandleRaycasting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Raycast
+            if(Physics.Raycast(cameraTransform.position, cameraTransform.forward, out RaycastHit hit))
+            {
+                if (hit.collider.CompareTag(enemy)) {
+                Debug.Log(hit.collider.gameObject.name);
+                Debug.DrawLine(cameraTransform.position + new Vector3(0, -0.1f, 0), hit.point, Color.red, 1f);
+                //Collect Parts?
+                Destroy(hit.collider.gameObject);
+                    Debug.Log("Destroyed: " + hit.collider.gameObject.name);
+                }
+            }
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.E) && charges > 0) {
-            timerRunning = true;
-            targetTime = 10.0f;
-            charges--;
-        } 
-        if (timerRunning && charges >= 0){
-            if (targetTime > 0){
-                enemySpeed = slowedSpeed;
-                Debug.Log(charges);
-                targetTime -= Time.deltaTime;
-            }
-        }
-        if (targetTime <= 0)
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) );
+        cameraTransform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0));
+        Vector3 move = Vector3.zero;
+        //Apply walk vector
+        move += Input.GetAxis("Vertical") * transform.forward;
+        move += Input.GetAxis("Horizontal") * transform.right;
+        move = move.normalized * WalkSpeed;
+        //Apply gravity
+        move += new Vector3(0, yspeed, 0);
+
+
+        Cc.Move(move * Time.deltaTime);
+        HandleRaycasting();
+        if (Input.GetKeyDown(KeyCode.R)) // Press 'R' to reset
         {
-            timerEnded();
+            ResetCurrentScene();
         }
-        transform.LookAt(player);
-        transform.Translate(enemySpeed * Time.deltaTime * Vector3.forward);
-    }
-    void timerEnded()
-    {
-        enemySpeed = baseSpeed;
-        targetTime = 0f;
-        timerRunning = false;
     }
 }
